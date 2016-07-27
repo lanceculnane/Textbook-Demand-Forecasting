04bcd8e924811f4ddfc10310aff1b9d91193518e
Alex Levenstein <alexlevenstein@Alexs-MacBook-Pro.local>
2016-07-26 21:47:29 -0600

    updated data cleaner

--- wrangler/data_cleaning.py
+++ wrangler/data_cleaning.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+import pickle
+import pandas as pd
 
 def categorize_terms(client,db='test_db2'):
     """
@@ -38,8 +40,9 @@ def convert_book_data(client,db='test_db2'):
     """
     collection = client[db].stores
     cursor = collection.find({'structure_complete':1,'books_complete':1},modifiers={"$snapshot": True})
-    rec_types = ['REQUIRED','REQUIRED PACKAGE','RECOMMENDED','RECOMMENDED PACKAGE','BOOKSTORE RECOMMENDED','PACKAGE COMPONENT','']
+    #rec_types = ['REQUIRED','REQUIRED PACKAGE','RECOMMENDED','RECOMMENDED PACKAGE','BOOKSTORE RECOMMENDED','PACKAGE COMPONENT','']
     for store in cursor:
+        print store['store_name']
         for campus in store['campuses']:
             for term in campus['terms']:
                 for department in term['departments']:
@@ -51,13 +54,18 @@ def convert_book_data(client,db='test_db2'):
                                 new_prices = []
                                 #isbn = book['isbn']
                                 #rec_type = book['rec_type']
-                                if (not ''.join(prices)):
+                                # if no book info, remove book from db
+                                if (len(prices) == 0 or prices[0]==''):
+                                    section['books'] = []
                                     continue
                                 for purchase_type,price in zip(purchase_types,prices):
                                     if price == 'nan':
                                         new_prices.append(-1)
+                                    elif (isinstance(price,float) or isinstance(price,int)):
+                                        new_prices.append(price)
                                     else:
-                                        new_prices.append(float(price.strip('$')))
+
+                                        new_prices.append(float(price.strip('$').strip(',')))
                                 book['buy_used'] = new_prices[0]
                                 book['buy_new'] = new_prices[1]
                                 book['rent_used'] = new_prices[2]
@@ -74,10 +82,57 @@ def convert_book_data(client,db='test_db2'):
             },
             upsert=False
         )
-def add_unitid(client):
-    return 0
-    
+
+def add_unitid(client, db='test_db2'):
+    """
+    Adds IPEDS unitid to each store so other IPEDS data can be added to the correct stores.  Should update to add all IPEDS data in one function
+    """
+    collection = client[db].stores
+    cursor = collection.find({'structure_complete':1,'books_complete':1},modifiers={"$snapshot": True})
+    school_map = pickle.load(open('/Users/alexlevenstein/Documents/projects/books/BNcollege/Rewrite/info/mapped_schools.pkl','rb'))
+    for store in cursor:
+        collection.update_one(
+            #{ '$unwind': '$campuses'},
+            #{ '$unwind': '$campuses.terms'},
+            {
+            '_id': store['_id']
+            },
+            {'$set':{
+                    'unitid':school_map[store['url']]
+                    }
+            },
+            upsert=False
+        )
+
+def add_ipeds_data(client,db='test_db2'):
+    all_schools_data = pd.read_csv('/Users/alexlevenstein/Documents/Galvanize/capstone/education_datasets/7-26-2016---812/Data_7-26-2016---812.csv')
+    collection = client[db].stores
+    cursor = collection.find({'structure_complete':1,'books_complete':1},modifiers={"$snapshot": True})
+    for store in cursor:
+        if store['unitid'] == 0:
+            continue
+        store_data = all_schools_data[all_schools_data['UnitID']==store['unitid']].values[0]
+        #print len(store_data)
+
+        collection.update_one(
+            {
+            '_id': store['_id']
+            },
+            {'$set':{
+                    'type' : "Point",
+                    'coordinates' : [store_data[4],store_data[5]],
+                    'ug_enrollment' : int(store_data[3]),
+                    'sfr' : store_data[2],
+                    'zip' : store_data[6],
+                    'state' : store_data[7]
+                    }
+            },
+            upsert=False
+        )
+
 if __name__=='__main__':
     client = MongoClient()
-    #categorize_terms(client,db='test_db2')
-    #convert_book_data(client)
+    #categorize_terms(client,db='test_db3')
+    #convert_book_data(client,db='test_db3')
+    #add_unitid(client,db='test_db3')
+    add_ipeds_data(client, db='test_db3')
